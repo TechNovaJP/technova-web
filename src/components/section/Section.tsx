c403ec2c533c4b86689a94567fd59250a47a3f17
SomaTakata <somatakata@gmail.com>
2024-06-02 01:11:23 +0900

    Sectionコンポーネントの追加

---
+++ src/components/section/Section.tsx
@@ -0,0 +1,38 @@
+import Image, { StaticImageData } from "next/image";
+import VerticalText from "@/components/VerticalItem/VerticalText";
+
+interface SectionProps {
+  title: string;
+  heading: string;
+  description: string;
+  image: StaticImageData;
+}
+
+const Section: React.FC<SectionProps> = ({
+  title,
+  heading,
+  description,
+  image,
+}) => {
+  return (
+    <section className="flex-col py-24 px-10 w-full flex items-start">
+      <VerticalText text={title} />
+      <div className="px-20">
+        <h2 className="whitespace-pre-line mb-10 tracking-widest text-4xl font-black leading-relaxed">
+          {heading}
+        </h2>
+        <p className="whitespace-pre-line mb-10 text-base tracking-[3px]">
+          {description}
+        </p>
+        <Image
+          src={image}
+          alt={`${title} image`}
+          priority
+          className="relative rounded-md"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Section;
