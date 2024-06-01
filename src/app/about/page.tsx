8cbdceb2c9c45ad5d7fb7a9a0869e07b543bd657
SomaTakata <somatakata@gmail.com>
2024-06-01 18:24:15 +0900

    layoutに移植

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,3 @@
-import Header from "../component/header";
-
 export default function About() {
-  return (
-    <div className="flex flex-col items-center">
-      <Header />
-    </div>
-  );
+  return <div className="flex flex-col items-center"></div>;
 }
