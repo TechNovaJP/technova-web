7f6a729ed9d82979163ee024c49c8c3a63a6e50a
SomaTakata <somatakata@gmail.com>
2024-06-02 15:23:02 +0900

    VerTicalText  レスポンシブをtailwind 仕様に変更

--- src/components/VerticalItem/VerticalText.tsx
+++ src/components/VerticalItem/VerticalText.tsx
@@ -6,7 +6,7 @@ interface VerticalTextProps {
 
 const VerticalText: React.FC<VerticalTextProps> = ({ text }) => {
   return (
-    <p className="border-r-2 border-black tracking-[1.5px] pr-2 [writing-mode:vertical-rl] font-black text-[18px] leading-none">
+    <p className="border-r-2 border-black tracking-[1.5px] pr-2 [writing-mode:vertical-rl] font-black sm:text-[18px] text-base leading-none">
       {text}
     </p>
   );
