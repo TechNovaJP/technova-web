b3754cccb7ecc65836a317b7d0c7d3ca6a07563b
SomaTakata <somatakata@gmail.com>
2024-06-04 00:40:12 +0900

    InfoPageの修正

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -4,6 +4,7 @@ import topImage from "./image/top-image.png";
 import VerticalText from "@/components/VerticalItem/VerticalText";
 import { getNews, getProducts } from "../../../libs/newt";
 import { News } from "../../../types/news";
+import InfoSection from "@/components/section/InfoSection";
 
 interface NewsProps {
   news: News[];
@@ -21,37 +22,7 @@ const MobileNav: React.FC<NewsProps> = ({ news }) => {
   return (
     <main className="flex min-h-screen  w-full flex-col items-center  pr-[50px] pb-28 font-text sm:hidden">
       <CenteredTextOverlay topImage={topImage} text="お知らせ" />
-      <div className="flex-col pt-16 pb-2 pl-4 pr-3 w-full flex items-start">
-        <VerticalText text="お知らせ" />
-      </div>
-
-      <div className="pl-6 pr-2 pt-8 w-full">
-        {news.map((news) => (
-          <>
-            <div
-              key={news._id}
-              className="w-full flex justify-between items-center bg-white pr-2 pl-4 pt-6 pb-1 border-t hover:border hover:shadow-md hover:rounded-lg"
-            >
-              <div className="flex items-center">
-                <Image
-                  src={news.image.src}
-                  alt={news.image.altText || "Profile Image"}
-                  className="object-contain border rounded-md"
-                  width={100}
-                  height={60}
-                />
-                <p className="ml-4 pr-4 font-bold">{news.title}</p>
-              </div>
-            </div>
-            <p className="text-sm pr-2 pb-1 font-bold w-full flex justify-end text-slate-300">
-              {formatDate(news.date)}
-            </p>
-          </>
-        ))}
-        <div className="w-full ">
-          <div className="w-full h-[0.3px] bg-slate-200 rounded-xl " />
-        </div>
-      </div>
+      <InfoSection title="お知らせ" id="news" news={news} />
     </main>
   );
 };
@@ -60,38 +31,13 @@ const DesktopNav: React.FC<NewsProps> = ({ news }) => {
   return (
     <main className="sm:flex min-h-screen  w-full flex-col items-center  px-[50px] pb-28 font-text hidden">
       <CenteredTextOverlay topImage={topImage} text="お知らせ" />
-      <div className="flex-col pt-16 pb-2 pl-4 pr-3 w-full flex items-start">
-        <VerticalText text="お知らせ" />
-      </div>
-
-      <div className="pl-14 pr-8 py-10 w-full">
-        {news.map((news) => (
-          <>
-            <div
-              key={news._id}
-              className="w-full flex justify-between items-center bg-white pr-2 pl-4 py-8 border-t hover:border hover:shadow-md hover:rounded-lg"
-            >
-              <div className="flex items-center">
-                <Image
-                  src={news.image.src}
-                  alt={news.image.altText || "Profile Image"}
-                  className="object-contain border rounded-md"
-                  width={100}
-                  height={100}
-                />
-                <p className="ml-4 pr-4 font-bold">{news.title}</p>
-              </div>
-              <div>
-                <p className="text-sm font-bold pr-2 text-slate-300">
-                  {formatDate(news.date)}
-                </p>
-              </div>
-            </div>
-          </>
-        ))}
-        <div className="w-full ">
-          <div className="w-full h-[0.3px] bg-slate-200 rounded-xl " />
-        </div>
+      <div className="w-full pl-6 pr-8">
+        <InfoSection
+          title="お知らせ"
+          id="news"
+          news={news}
+          displayCount={0} // 表示するニュースの数を指定
+        />
       </div>
     </main>
   );
@@ -99,7 +45,6 @@ const DesktopNav: React.FC<NewsProps> = ({ news }) => {
 
 export default async function Info() {
   const news = await getNews();
-  console.log(news);
 
   return (
     <>
